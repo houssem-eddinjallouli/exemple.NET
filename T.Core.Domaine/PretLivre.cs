563a8ea14ec78a0ba8f0eb185e2793369c667842
houssem-eddinjallouli <houssem-eddin.jallouli@esprit.tn>
2023-11-26 20:38:12 +0100

    test

--- T.Core.Domaine/PretLivre.cs
+++ T.Core.Domaine/PretLivre.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace T.Core.Domaine
-{//class de relation
+{
     public class PretLivre
     {
         public DateTime DateFin { get; set; }
